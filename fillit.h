fb3e9250836ce59e99e76114dfe402b43b8c6d63
Jyri Rummukainen <jrummuka@c2r6p11.hive.fi>
2021-12-17 15:15:59 +0200

    lib with only includes

---
+++ fillit.h
@@ -0,0 +1,11 @@
+#ifndef FILLIT_H
+# define FILLIT_H
+
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "libft/libft.h"
+#include <string.h>
+#include <sys/stat.h>
+#include <stdio.h> //REMEMBER TO DELETE, JUST FOR TESTPRINTS
+
